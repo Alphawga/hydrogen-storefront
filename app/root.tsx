31a27e998926c2a0676226b49a17b94a25e02372
ALPHAWGA BAMIDELE <alphawgas@gmail.com>
2023-08-29 11:19:45 +0100

    Setup Tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,7 @@ import favicon from '../public/favicon.svg';
 import resetStyles from './styles/reset.css';
 import appStyles from './styles/app.css';
 import {Layout} from '~/components/Layout';
+import tailwindCss from './styles/tailwind.css';
 
 // This is important to avoid re-fetching root queries on sub-navigations
 export const shouldRevalidate: ShouldRevalidateFunction = ({
@@ -40,6 +41,7 @@ export const shouldRevalidate: ShouldRevalidateFunction = ({
 
 export function links() {
   return [
+    {rel: 'stylesheet', href: tailwindCss},
     {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
     {
